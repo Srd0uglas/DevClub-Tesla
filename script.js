d85818f943e971ed5b5248b1fe666c30779dd9ea
Douglas Souza <srd0uglas337@gmail.com>
2024-07-12 17:45:47 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // indo até o html, buscando a classe "form" e guardando na variável "formulario"
 let formulario = document.querySelector(".form")
+let formulario2 = document.querySelector(".form2")
 
 function aparecerForm(){
     // por padrão ele é left -50(sumir) e a funcção vai mudar para 50% (aparecer)
-    formulario.style.left = "50%"
+    formulario.style.left = "5%"
+    
 }
 
 function desapareceForm(){
      formulario.style.left = "-50%"
+}
+
+
+
+
+
+function aparecerForm2(){
+    formulario2.style.left = "67%"
+}
+
+function desapareceForm2(){
+    formulario2.style.left = "-67%"
 }
